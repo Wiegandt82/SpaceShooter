f96421c4e6441ea32de36c4d0c1630128c1b6168
Wiegandt82 <wiegandt@hotmail.com>
2023-07-19 23:21:51 +0200

    2. Crash seequence

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {  
-    void OnCollisionEnter(Collision other)   
-    {
-        Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
-    }
-    
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        startCrashSeequence();
+    }
+
+    void startCrashSeequence()
+    {
+        SceneManager.LoadScene(1);
     }
 }
