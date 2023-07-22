828fd3c951aa580b8097497191befe2d9cc7ef7e
Wiegandt82 <wiegandt@hotmail.com>
2023-07-22 08:03:34 +0200

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
 
     ScoreBoard scoreBoard;
 
@@ -18,11 +19,16 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     private void ProcessHit()
     {
+        hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
 
